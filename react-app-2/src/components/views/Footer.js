fd7d8a85cde5a302a864d40a4b62d7944937fbf8
Jakub Wilga <wilga327@wp.pl>
2023-05-16 19:47:55 +0200

    add styles

--- react-app-2/src/components/views/Footer.js
+++ react-app-2/src/components/views/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <div>
-        <p>Footer</p>
+        <p className="text-center text-muted mt-4 mb-4">Copyright &copy; BlogApp 2022</p>
       </div>
     );
 };
